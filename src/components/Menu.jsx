f9efdf3d65bd5ce2400a1cc67582b35e1fb57b9d
kwrona05 <kwrona05@gmail.com>
2025-01-18 12:58:27 +0100

    Add all elements (div buttons) to menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,27 @@
+import SearchBar from "./menu-components/SearchBar";
+
+const Menu = () => {
+  return (
+    <div className="menu-container">
+      <div className="buttons-container">
+        <div className="heart-data-btn" onClick={() => console.log("clicked")}>
+          Heart data
+        </div>
+        <div
+          className="historical-charts"
+          onClick={() => console.log("clicked")}
+        >
+          Charts
+        </div>
+        <div className="place-check" onClick={() => console.log("clicked")}>
+          Search place
+        </div>
+        <div className="user-data" onClick={() => console.log("clicked")}>
+          Your profile
+        </div>
+      </div>
+      <SearchBar />
+    </div>
+  );
+};
+export default Menu;
