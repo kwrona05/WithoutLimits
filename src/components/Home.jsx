4548e5af022b09a9bd145553ba8581920b346410
kwrona05 <kwrona05@gmail.com>
2025-01-18 12:56:52 +0100

    Add menu component to Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,2 +1,8 @@
-const Home = () => {};
+import Menu from "./Menu";
+
+const Home = () => {
+  return <Menu />;
+  //Mapa
+  //Bar do dodawania lub oznaczania jako nieaktualne nowych miejsc
+};
 export default Home;
