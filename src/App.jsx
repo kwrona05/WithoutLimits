f46be64e2396777d811270c9c0775a8d65bb2621
kwrona05 <kwrona05@gmail.com>
2025-01-19 15:32:09 +0100

    configure router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/menu-components/Login";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 export default App;
