af081545cd43efb00cabb834aa6226fbccb262c2
kwrona05 <kwrona05@gmail.com>
2025-01-19 12:11:21 +0100

    Fix structure in Home.jsx
    Insert Menu component in div menu-container. Insert Map component in div map-container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import Menu from "./Menu";
+import Map from "./Map";
 
 const Home = () => {
-  return <Menu />;
+  return (
+    <div className="container">
+      <div className="menu-container">
+        <Menu />
+      </div>
+      <div className="map-container">
+        <Map />
+      </div>
+    </div>
+  );
   //Mapa
   //Bar do dodawania lub oznaczania jako nieaktualne nowych miejsc
 };
