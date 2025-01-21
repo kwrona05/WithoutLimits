0e63be93110f12d5ee3cc426d26453b53bac61a2
kwrona05 <kwrona05@gmail.com>
2025-01-21 11:33:51 +0100

    Remove unnesessary route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
