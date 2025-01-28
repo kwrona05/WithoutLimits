31f987ebf3ecb1f931e213a4f53fdf8d49284f4e
kwrona05 <kwrona05@gmail.com>
2025-01-28 09:38:39 +0100

    Create Search bar component

--- src/components/menu-components/SearchBar.jsx
+++ src/components/menu-components/SearchBar.jsx
@@ -1,9 +1,19 @@
+import { useRef } from "react";
 import styles from "./SearchBar.module.scss";
 const SearchBar = () => {
+  const inputRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputRef.current.value);
+  };
+  //In the future search has to navigate to the city
   return (
     <div className={styles.container}>
-      <input placeholder="Enter the city" type="text" />
-      <button type="submit">Search</button>
+      <input ref={inputRef} placeholder="Enter the city" type="text" />
+      <button type="submit" onClick={handleSubmit}>
+        Search
+      </button>
     </div>
   );
 };
