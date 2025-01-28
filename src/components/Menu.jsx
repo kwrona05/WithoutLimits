b50c2e3b05c6db4fc0eddef7bc68601100bf3cc7
kwrona05 <kwrona05@gmail.com>
2025-01-28 09:39:46 +0100

    Create router in Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,16 +3,32 @@ import SearchBar from "./menu-components/SearchBar";
 import styles from "./Menu.module.scss";
 
 const Menu = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.menuContainer}>
       <div className={styles.buttonContainer}>
-        <div className={styles.divBtn} onClick={() => console.log("clicked")}>
+        <div
+          className={styles.divBtn}
+          onClick={() => {
+            navigate("/home/heart-data");
+          }}
+        >
           Heart data
         </div>
-        <div className={styles.divBtn} onClick={() => console.log("clicked")}>
+        <div
+          className={styles.divBtn}
+          onClick={() => {
+            navigate("/home/char");
+          }}
+        >
           Charts
         </div>
-        <div className={styles.divBtn} onClick={() => console.log("clicked")}>
+        <div
+          className={styles.divBtn}
+          onClick={() => {
+            navigate("/home/profile");
+          }}
+        >
           Your profile
         </div>
       </div>
